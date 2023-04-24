63b911f2ffdd48a31af28ed1e1eb1cfaa6c0c008
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-24 16:11:40 +0530

    retrieve data from payroll_service table

--- PayrollService.sql
+++ PayrollService.sql
@@ -16,10 +16,12 @@ Name varchar(255), Salary float,
 StartDate Date
 )          
 
-select * from employee_payroll    
-
+/* insert new records in table */
 insert into employee_payroll values ('Siddique', 30000.00, '2020-03-24'),
                                     ('Radha', 35000.00, '2021-06-20'),
 									('Mahek', 25000.00, '2020-10-16'),
 									('Ayesha', 37000.00, '2022-09-30'),
 									('Roman', 42000.00, '2019-01-07')
+
+/*  retrieve all data from table */
+select * from employee_payroll    
