308770cda3739b6fb45aede08ea6c15c8f119100
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-24 15:59:51 +0530

    create table

--- PayrollService.sql
+++ PayrollService.sql
@@ -7,3 +7,12 @@ create database payroll_service /*create database*/
 select name from sys.databases /*show database */
 
 use payroll_service /* use database */
+
+/* create table with fields */
+create table employee_payroll
+(
+ID int Primary key identity, 
+Name varchar(255), Salary float, 
+StartDate Date
+)          
+
