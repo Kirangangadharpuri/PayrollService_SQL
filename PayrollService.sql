57e4890d1289611f32bb0a54ee77be1effc6512e
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-24 15:53:25 +0530

    create database

--- PayrollService.sql
+++ PayrollService.sql
@@ -1,3 +1,9 @@
 
 
 /* Welcome to the PayRoll Service Problem */
+
+create database payroll_service /*create database*/
+
+select name from sys.databases /*show database */
+
+use payroll_service /* use database */
